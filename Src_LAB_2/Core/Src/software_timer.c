d28d20c4f815f949e66cfa04023434ce8ebd2715
sonsonha <ha.danh2002@hcmut.edu.vn>
2023-10-31 15:07:50 +0700

    ex10

--- Src_LAB_2/Core/Src/software_timer.c
+++ Src_LAB_2/Core/Src/software_timer.c
@@ -16,6 +16,9 @@ int timer2_flag = 0;
 int timer3_counter = 0;
 int timer3_flag = 0;
 
+int timer4_counter = 0;
+int timer4_flag = 0;
+
 
 void setTimer1(int duration) {
 	timer1_counter = duration;
@@ -30,6 +33,11 @@ void setTimer2(int duration) {
 void setTimer3(int duration) {
 	timer3_counter = duration;
 	timer3_flag = 0;
+}
+
+void setTimer4(int duration) {
+	timer4_counter = duration;
+	timer4_flag = 0;
 }
 
 
@@ -53,6 +61,13 @@ void timerRun() {
 			 timer3_counter--;
 			 if(timer3_counter <= 0) {
 				 timer3_flag = 1;
+			 }
+		}
+
+		if (timer4_counter > 0) {
+			 timer4_counter--;
+			 if(timer4_counter <= 0) {
+				 timer4_flag = 1;
 			 }
 		}
 }
