1223afd1e4f6d5384207653cb722c03a04737045
sonsonha <ha.danh2002@hcmut.edu.vn>
2023-11-01 09:33:35 +0700

    ex8 update

---
+++ Src_LAB_2/Core/Inc/update_led.h
@@ -0,0 +1,14 @@
+/*
+ * update_led.h
+ *
+ *  Created on: Nov 1, 2023
+ *      Author: sonha
+ */
+
+#ifndef INC_UPDATE_LED_H_
+#define INC_UPDATE_LED_H_
+
+void display7SEG ( int num);
+
+
+#endif /* INC_UPDATE_LED_H_ */
